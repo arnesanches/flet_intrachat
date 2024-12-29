1d6fedf4640862ce0854d67d537bf50066156991
arnesanches <arneantonio@hotmail.com>
2024-12-29 19:41:48 -0300

    Adiciona canal PubSub e função para exibir mensagens no chat

--- flet_intrachat.py
+++ flet_intrachat.py
@@ -8,3 +8,11 @@ def main(pagina):
     # Títulos e mensagens
     titulo = ft.Text("Flet IntraChat", size=30, color="#2980B9", weight="bold")  # Título principal
     titulo_janela = ft.Text("Bem-vindo(a) ao Flet IntraChat!", color="#2980B9", weight="bold")  # Texto do popup inicial
+
+    # Função para exibir mensagens no chat ao receber dados pelo canal PubSub
+    def enviar_mensagem_tunel(mensagem):
+        chat.controls.append(ft.Text(mensagem, color="#34495E"))  # Adiciona mensagem ao chat
+        pagina.update()  # Atualiza a interface
+
+    pagina.pubsub.subscribe(enviar_mensagem_tunel)  # Inscreve a função no canal de mensagens
+
