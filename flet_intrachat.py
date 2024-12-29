5250807d0eec9d4a4455915ebdba9ee3efb522b6
arnesanches <arneantonio@hotmail.com>
2024-12-29 19:43:52 -0300

    Criação do campo de mensagens e botão de envio

--- flet_intrachat.py
+++ flet_intrachat.py
@@ -16,3 +16,12 @@ def main(pagina):
 
     pagina.pubsub.subscribe(enviar_mensagem_tunel)  # Inscreve a função no canal de mensagens
 
+        # Campo para digitar mensagens
+    texto_mensagem = ft.TextField(label="Digite sua mensagem:", on_submit=enviar_mensagem,
+                                   bgcolor="#FFFFFF", border_color="#BDC3C7", color="#000000")
+    botao_enviar = ft.ElevatedButton("Enviar", on_click=enviar_mensagem,  # Botão para enviar mensagem
+                                      bgcolor="#2980B9", color="white")  # Azul corporativo
+
+    chat = ft.Column(scroll="always")  # Contêiner das mensagens com scroll habilitado
+    linha_mensagem = ft.Row([texto_mensagem, botao_enviar])  # Combinação do campo e botão "Enviar"
+
